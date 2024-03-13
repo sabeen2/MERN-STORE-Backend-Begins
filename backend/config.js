15857b26645ec8fba096cbd0c9278c83edd4ca6c
sabeen2 <sabeenpandey2@gmail.com>
2024-03-13 22:31:56 +0545

    first mongoose schema

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555
+
+export const MongoDBURL= "mongodb+srv://root:root%40sabeen@mern-book-store.9wvfjgl.mongodb.net/MERN-BOOK-STORE?retryWrites=true&w=majority&appName=MERN-BOOK-STORE"
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,36 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, MongoDBURL } from "./config.js";
+import mongoose from 'mongoose';
+import { Book } from "./models/bookModel.js";
 
 
 const app = express();
 
-app.listen(PORT, ()=> {
-    console.log(`Hello Sabin Welcome to backend port : ${PORT}`)
-})
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send(`Welcome hello zzz`);
+});
+
+
+app.post("/books", async (request, response)=>{
+
+  try {
+
+    
+  } catch (error) {
+    console.log(error.message)
+    response.status(500).send({message: error.message})
+    
+  }
+});
+
+mongoose.connect(MongoDBURL)
+  .then(() => {
+    console.log("App is connected");
+    app.listen(PORT, () => {
+        console.log(`Hello Sabin Welcome to backend port : ${PORT}`);
+      });      
+  })
+  .catch((error) => {
+    console.error(error);
+  });
